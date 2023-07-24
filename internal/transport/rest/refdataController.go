f862a175e891e20374d22e335107d61871c043c4
mr.w1zard@mail.ru <mr.w1zard@mail.ru>
2023-07-24 15:35:02 +0600

    Add refdata controller

---
+++ internal/transport/rest/refdataController.go
@@ -0,0 +1,49 @@
+package rest
+
+import (
+	"fmt"
+	"rkeeper7-simpleapi-service/internal/config"
+	XmlRkeeper "rkeeper7-simpleapi-service/internal/xmlRkeeper"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetRefData(c *fiber.Ctx) error {
+	req := HttpRequestData{
+		CashServerIP:   config.ServerConfig.Config.RKeeperCashServerIp,
+		CashServerPort: config.ServerConfig.Config.RkeeperCashServerPort,
+		Method:         "POST",
+		Username:       config.ServerConfig.Config.RkeeperUser,
+		Password:       config.ServerConfig.Config.RkeeperPassword,
+	}
+	// Request xml body
+	xmlData, err := XmlRkeeper.RequestGetRefData("MenuItems", "")
+	if err != nil {
+		return err
+	}
+	req.Payload = xmlData
+	//Send http request to Rkeeper7
+	body, err := HttpRequestToRkeeper(req)
+
+	if err != nil {
+		fmt.Println("err:", err)
+		return c.JSON(
+			&fiber.Map{
+				"data":  body,
+				"error": err.Error(),
+			},
+		)
+	}
+	// resp, err := XmlRkeeper.ResponseGetSystemInfo(body)
+	// if err != nil {
+	// 	fmt.Println("err:", err)
+	// 	return c.JSON(
+	// 		&fiber.Map{
+	// 			"data":  body,
+	// 			"error": err.Error(),
+	// 		},
+	// 	)
+	// }
+	// Обработка ответа
+	return c.JSON(fiber.Map{"data": string(body)})
+}
