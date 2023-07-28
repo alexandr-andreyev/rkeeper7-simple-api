03b446a339473f30e1a1c61289cb3580b724baf2
mr.w1zard@mail.ru <mr.w1zard@mail.ru>
2023-07-28 17:56:24 +0600

    Функция переделана на получение блюд меню через клиент

--- internal/transport/rest/refdataController.go
+++ internal/transport/rest/refdataController.go
@@ -4,43 +4,36 @@ import (
 	"fmt"
 	"rkeeper7-simpleapi-service/internal/config"
 	XmlRkeeper "rkeeper7-simpleapi-service/pkg/xmlRkeeper-api"
-	XMLRK7Models "rkeeper7-simpleapi-service/pkg/xmlRkeeper-api/models"
+	"rkeeper7-simpleapi-service/pkg/xmlRkeeper-api/Rk7CMD"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func GetRefData(c *fiber.Ctx) error {
-	req := HttpRequestData{
-		CashServerIP:   config.ServerConfig.Config.RKeeperCashServerIp,
-		CashServerPort: config.ServerConfig.Config.RkeeperCashServerPort,
-		Method:         "POST",
-		Username:       config.ServerConfig.Config.RkeeperUser,
-		Password:       config.ServerConfig.Config.RkeeperPassword,
-	}
+func GetMenuItems(c *fiber.Ctx) error {
+	Client := XmlRkeeper.NewClient(
+		config.ServerConfig.Config.RKeeperCashServerIp,
+		config.ServerConfig.Config.RkeeperCashServerPort,
+		config.ServerConfig.Config.RkeeperUser,
+		config.ServerConfig.Config.RkeeperPassword)
+
 	// Request xml body
-	reqData := new(XMLRK7Models.RequestGetRefData)
+	reqData := new(Rk7CMD.RequestGetRefData)
 	reqData.CMD = "GetRefData"
 	reqData.RefName = "MenuItems"
-	reqData.IgnoreEnums = "1"
 	reqData.WithChildItems = "1"
-	reqData.WithMacroProp = "1"
-	xmlData, err := XmlRkeeper.RequestGetRefData(reqData)
-	if err != nil {
-		return err
-	}
-	req.Payload = xmlData
-	//Send http request to Rkeeper7
-	body, err := HttpRequestToRkeeper(req)
+
+	// Request xml body
+	resp, err := Client.GetRefData(*reqData)
 
 	if err != nil {
 		fmt.Println("err:", err)
 		return c.JSON(
 			&fiber.Map{
-				"data":  body,
+				"data":  resp,
 				"error": err.Error(),
 			},
 		)
 	}
 
-	return c.JSON(fiber.Map{"data": string(body)})
+	return c.JSON(resp)
 }
