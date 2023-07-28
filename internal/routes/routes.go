132772a323bc073e6c9bb59ad1f429ea588e1ff8
mr.w1zard@mail.ru <mr.w1zard@mail.ru>
2023-07-28 17:54:28 +0600

    Добавлен префикс апи, маршрут для групп меню

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -7,6 +7,8 @@ import (
 )
 
 func Setup(app *fiber.App) {
-	app.Get("/api/v1/systeminfo", rest.GetInfo)
-	app.Post("/api/v1/refdata", rest.GetRefData)
+	api := app.Group("api/v1")
+	api.Get("/systeminfo", rest.GetInfo)
+	api.Post("/refdata", rest.GetMenuItems)
+	api.Post("/categlist", rest.GetCategList)
 }
